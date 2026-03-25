6e98d5744767ea99c9acbac1ace19958416fe999
yamapanse <t-yamashita@ymp-jp.com>
2026-03-25 09:02:55 +0900

    Modified

--- next.config.js
+++ next.config.js
@@ -1,27 +1,12 @@
-const withPWA = require('@ducanh2912/next-pwa').default;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // ここに型エラー無視の設定を追加します
   typescript: {
     ignoreBuildErrors: true,
   },
-  // ついでにESLintのエラーも無視するようにしておくと安心です
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-// PWAの設定でnextConfigを包んで書き出します
-module.exports = withPWA({
-  dest: 'public',
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  // Vercel Edge runtime と非互換のため本番でも一時的に無効化
-  disable: true,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-})(nextConfig);
+module.exports = nextConfig;
